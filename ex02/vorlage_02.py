3fc22a65c97b491527c31d87833e8b39637795c7
Lukas <lukas.vollenweider@bluewin.ch>
2015-10-09 19:04:04 +0200

    added authors

--- ex02/vorlage_02.py
+++ ex02/vorlage_02.py
@@ -5,12 +5,15 @@
 # PCL1-Ü2-Aufgabe 5
 # Vorlage von Raphael Balimann (raphael.balimann@uzh.ch) - HS 2015
 #
-
+# Authors:
+# Bill Bosshard (12-933-255)
+# Lukas Vollenweider (13-751-888)
+#
 
 # Eingabe des Names vom Nutzer, jedoch nur Nachname, danach Begrüssung durch Programm
 lastname = raw_input('Bitte gebe deinen Nachnamen ein: ')
-surname = raw_input('Bitte gebe deinen Vornamen ein: ')
-print "Hallo", surname, lastname
+firstname = raw_input('Bitte gebe deinen Vornamen ein: ')
+print "Hallo", firstname, lastname
 
 # Vermessung der Eingabe
 length = len(lastname)
@@ -21,6 +24,6 @@ print "Ihr Nachname hat", length, "Zeichen"
 if (length >= 8):
 	print "Auf Wiedersehen", lastname
 elif ((length >= 5) & (length < 8)):
-	print "Auf Wiedersehen", surname, lastname
+	print "Auf Wiedersehen", firstname, lastname
 else:
-	print "Auf Wiedersehen", surname
+	print "Auf Wiedersehen", firstname
