0a781063db996d7721c63e8b914a4691ab7cf166
Lukas <lukas.vollenweider@bluewin.ch>
2015-12-09 11:48:17 +0100

    solution task 1.2

---
+++ bilbos_lukasvollenweider_pcl1_6/aufgabe1_2.py
@@ -0,0 +1,104 @@
+import unicodedata
+
+class Normalstring(object):
+
+	def __init__(self, string):
+		"""
+		Normalstring initializer
+
+		Saves the string to a local variable 'unnormal'. 
+		Also saves a normalized version of the input string in a local variable 'normal'
+
+		Args:
+			string: unicode string
+
+		Returns:
+			a Normalstring instance
+		"""
+
+		self.unnormal = string
+		self.normal = self._normalize(string)
+
+	def __eq__(self, other):
+		"""
+		Two Normalstrings are equal if there normalized version are equal
+
+		Args:
+			other: Normalstring to compare against
+
+		Returns:
+			true: if the strings are equal
+			false: if not
+		"""
+
+		return self.normal == other.normal
+
+	def __getitem__(self, i):
+		"""
+		Returns the normalized character from a Normalstring at position i 
+
+		Args:
+			String: normalized version of a string
+			i: Position of the character
+
+		Returns:
+			Character at postion i
+		"""
+
+		return self.normal[i]
+
+	def __contains__(self, other):
+		"""
+		Checks if self contains the other string
+
+		Returns:
+			True is self contains other
+			False otherwise
+		"""
+		if (self.index(other) != -1):
+			return True
+		else:
+			return False
+
+	def __str__(self):
+		"""
+		String representation of Normalstring
+
+		Returns:
+			the normalized version of a string
+		"""
+		return self.normal
+
+	def _normalize(self, string):
+		"""
+		Normalizes a unicode string
+
+		Args:
+			string: unicode string
+
+		Returns:
+			a normalized string
+		"""
+		nfkd_form = unicodedata.normalize('NFKD', string)
+		return u"".join(c for c in nfkd_form if not unicodedata.combining(c))
+
+	def index(self, string):
+		"""
+		Return the position of the first occurrence of a substring
+
+		Args:
+			string: Substring from which we want to know the position
+
+		Returns:
+			Positon if the string was found
+			-1 otherwise
+		"""
+
+		normalized_substring = self._normalize(string)
+		substring_length = len(normalized_substring)
+
+		for i in range(0, len(self.normal)):
+			if (self.normal[i:i + substring_length] == normalized_substring):
+				return i
+		return -1
+
